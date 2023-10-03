e51a8736d81a5a17ab12c121ff13f020fc30aa85
kooota0818 <131091962+kooota0818@users.noreply.github.com>
2023-10-03 09:56:49 +0900

    Create myjs.js

---
+++ myjs.js
@@ -0,0 +1,11 @@
+alert("hello,Osamu");
+alert(1+1);
+alert(Math.sqrt(1+1));
+alert("1.414"+"1.732");
+alert("1"/"0");
+alert("0"/"0");
+alert(2+2);
+alert("2"+"3");
+alert(Number("2")+Number("3"));
+alert("2")+(3);
+alert(Numbere("2")+(3));
