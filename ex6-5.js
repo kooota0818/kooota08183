00af89b3e713dbac56c3c23ad4b1e620bf8cea89
kooota0818 <131091962+kooota0818@users.noreply.github.com>
2023-11-07 10:42:01 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -5,6 +5,7 @@ function taro65(){who65="太郎";}
 function hanako65(){who65="花子";}
 
 var who66="world";
+function sayhello66(){alert("hello,"+who66+"!");}
 function sayhello66(){alert(who66+'が来た!');}
 function taro66(){alert(who66='太郎');}
 function hanako66(){alert(who66='花子');}
