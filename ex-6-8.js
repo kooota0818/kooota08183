eb723ca1b8496c49c0ed5b8fcbf534a0c6dff957
kooota0818 <131091962+kooota0818@users.noreply.github.com>
2023-11-14 09:59:55 +0900

    Create ex-6-8.js

---
+++ ex-6-8.js
@@ -0,0 +1,5 @@
+function sayhello68()
+alert('hello,+document.getElementByld('who68')'innerHTML+'!');
+}
+function taro68(){document.getElementByld('who68')'innerHTML='太郎';}
+function taro68(){document.getElementByld('who68')'innerHTML='花子';}
