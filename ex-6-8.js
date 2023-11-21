24de3341e60ef97cbd42b814d6dea25de944a660
kooota0818 <131091962+kooota0818@users.noreply.github.com>
2023-11-21 09:57:17 +0900

    Update ex-6-8.js

--- ex-6-8.js
+++ ex-6-8.js
@@ -3,4 +3,4 @@ alert('hello,+document.getElementByld('who68')'innerHTML+'!');
 }
 function taro68(){document.getElementByld('who68')'innerHTML='太郎';}
 function taro68(){document.getElementByld('who68')'innerHTML='花子';}
-function gazzo688(){document.getElementByld('who688').innerHTML='<imb src="./image/keiei_small.jpg" alert"josai">';} 
+function gazzo688(){document.getElementByld('who688').innerHTML='<imb src="./png-transparent-josai-university-hd-logo.png alert"オン">';} 
