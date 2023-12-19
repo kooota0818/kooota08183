cb6ad579b9b9399ab9b65f5221b4d1c33b738e77
kooota0818 <131091962+kooota0818@users.noreply.github.com>
2023-12-19 10:19:43 +0900

    Update ex6-12.js

--- ex6-12.js
+++ ex6-12.js
@@ -10,7 +10,7 @@ document.timer.moji.value=str.substr(0,cnt+1);}
   else{
     //全て表示されたら、空文字に戻す
 document.timer.moji.value="";}}
-function starfnc(){
+function startfnc(){
   //関数hyoji()を1000ミリ秒間隔で呼び出す
 setlnterval("hyoji()",1000);}
 
