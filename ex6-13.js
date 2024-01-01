0cb1a64a59f9d75166b6f20ea363490816507031
kooota0818 <131091962+kooota0818@users.noreply.github.com>
2024-01-01 15:32:10 +0900

    Update ex6-13.js

--- ex6-13.js
+++ ex6-13.js
@@ -13,7 +13,7 @@ var cnt13=document.timer13.moji13.value.length;
 if(cnt13<11)
 {
 //現在より1文字多く切り出して表示
-document.timer13.moji13.value=str13.sudstr(0,cnt13+1);}else{
+document.timer13.moji13.value=str13.substr(0,cnt13+1);}else{
 //すべて表示されたら空文字に戻す
 document.timer13.moji13.value ="";}
 //seTimeout()を含む関数を呼び出す
