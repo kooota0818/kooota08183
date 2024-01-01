262a3e5ae9c696bfee31836df35450b5987edad3
kooota0818 <131091962+kooota0818@users.noreply.github.com>
2024-01-01 15:30:44 +0900

    Update ex6-13.js

--- ex6-13.js
+++ ex6-13.js
@@ -10,7 +10,7 @@ var str13="一文字ずつ表示します";
 //テキストボックスの文字数
 var cnt13=document.timer13.moji13.value.length;
 //文字が全部表示されているか確認
-if(cnt13>11)
+if(cnt13<11)
 {
 //現在より1文字多く切り出して表示
 document.timer13.moji13.value=str13.sudstr(0,cnt13+1);}else{
