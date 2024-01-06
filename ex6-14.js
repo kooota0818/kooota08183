f2bf7fafe7197b6e323eff954c82f2e3c95faf01
kooota0818 <131091962+kooota0818@users.noreply.github.com>
2024-01-06 15:28:19 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,6 +1,6 @@
 function imagechange5() {
-    document.getElementById('logo3').src = './image/keiei_small.jpg';
+    document.getElementById('logo3').src = './image/josai.jo.jpg';
 }
 function imagechange6() {
-    document.getElementById('logo3').src = './image/josai.jo.jpg';
+    document.getElementById('logo3').src = './image/keiei_small.jpg';
 }
