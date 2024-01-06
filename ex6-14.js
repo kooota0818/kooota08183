62221dee637852d10e040b04717cab12dbe88037
kooota0818 <131091962+kooota0818@users.noreply.github.com>
2024-01-06 15:20:56 +0900

    Create ex6-14.js

---
+++ ex6-14.js
@@ -0,0 +1,6 @@
+function imagechange5() {
+    document.getElementById('logo3').src = './image/bluebird_baka.png';
+}
+function imagechange6() {
+    document.getElementById('logo3').src = './image/bird_aoitori_bluebird.png';
+}
