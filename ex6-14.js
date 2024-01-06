c95b21be3adb72244f1a8624a11c412559313668
kooota0818 <131091962+kooota0818@users.noreply.github.com>
2024-01-06 15:21:23 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,5 +1,5 @@
 function imagechange5() {
-    document.getElementById('logo3').src = './image/bluebird_baka.png';
+    document.getElementById('logo3').src = './image/keiei';
 }
 function imagechange6() {
     document.getElementById('logo3').src = './image/bird_aoitori_bluebird.png';
