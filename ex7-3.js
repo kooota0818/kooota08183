9f1ce4e9ede0b9cf339ebd40c617af977133af97
kooota0818 <131091962+kooota0818@users.noreply.github.com>
2024-01-07 14:11:27 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,1 +1,8 @@
-
+function bmi() 
+{
+    var a = Number(document.getElementById('input3').value);
+    var b = Number(document.getElementById('input4').value);
+    {
+        alert(a / (b * b));
+    }
+}
